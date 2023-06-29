70e7782a07c7adc875d9958ffe43a37e30cb7f8a
hardli <1060111768@qq.com>
2023-06-29 17:52:26 +0200

    update coords shape

--- data_visualization.py
+++ data_visualization.py
@@ -15,46 +15,48 @@ from util.utils import get_prepared, calc_axis_limit, plot_func
 parser = argparse.ArgumentParser(description='Visualize original csv data')
 
 ### general ###
-parser.add_argument('--data_path', type=str, default='dataset/chor2_20230609')
-# parser.add_argument('--data_path', type=str, default='dataset/testset_20230627')
+# parser.add_argument('--data_path', type=str, default='dataset/chor2_20230609')
+parser.add_argument('--data_path', type=str, default='dataset/testset_20230627')
 
 ### animation ###
-parser.add_argument('--start_frame', type=int, default=10000, help='from which frame to start visualize')
-parser.add_argument('--end_frame', type=int, default=10300, help='to which frame to end visualize')
-parser.add_argument('--output_anim', type=bool, default=True, help='whether to output animation of visualization')
+parser.add_argument('--start_frame', type=int, default=3400, help='from which frame to start visualize')
+parser.add_argument('--end_frame', type=int, default=4600, help='to which frame to end visualize')
+parser.add_argument('--output_anim', type=bool, default=False, help='whether to output animation of visualization')
 
 args = parser.parse_args([])
 
-input_path = os.path.join(args.data_path,'unknown.NoHead.csv')
-coords_ori,dist_time,joints_dict = get_prepared(input_path,frame_range=[args.start_frame,args.end_frame])
-N_frames = args.end_frame - args.start_frame
+if __name__ == '__main__':
 
-##################
-###### plot ######
-##################
+    input_path = os.path.join(args.data_path,'unknown.NoHead.csv')
+    coords,dist_time,joints_dict = get_prepared(input_path,frame_range=[args.start_frame,args.end_frame])
+    N_frames = args.end_frame - args.start_frame
 
-fig = plt.figure(figsize=(16,8))
-fig.tight_layout()
+    ##################
+    ###### plot ######
+    ##################
 
-# ax1: original coordinates in 3D
-ax1 = fig.add_subplot(1, 2, 1, projection='3d')
-ax1.view_init(30, 150)
-limits_1 = calc_axis_limit(coords_ori) # (x_high, x_low), (y_high, y_low), (z_high, z_low)
-title_1 = f'Original coordinates in frames {args.start_frame}-{args.end_frame}'
+    fig = plt.figure(figsize=(16,8))
+    fig.tight_layout()
 
-# ax2: distance changing in 2D
-ax2 = fig.add_subplot(1, 2, 2)
-x = np.arange(N_frames)
-dist_plots = [ax2.plot(x, dist_time[idx],label=f'{links}')[0] for idx,(links,joints) in enumerate(joints_dict.items())]
-plt.legend()
-title_2 = f'Distances of limbs in frames {args.start_frame}-{args.end_frame}'
+    ### ax1: original coordinates in 3D ###
+    ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+    ax1.view_init(30, 150)
+    high_1, low_1 = calc_axis_limit(coords) # (x_high, x_low), (y_high, y_low), (z_high, z_low)
+    title_1 = f'Original coordinates in frames {args.start_frame}-{args.end_frame}'
 
-# animation
-ani1 = animation.FuncAnimation(fig,plot_func,frames=N_frames,fargs=(ax1,joints_dict,coords_ori,limits_1,title_1,
-                                                                         ax2,dist_plots,dist_time,title_2),interval=17)
-plt.show()
+    #### ax2: distance changing in 2D ###
+    ax2 = fig.add_subplot(1, 2, 2)
+    x = np.arange(N_frames)
+    dist_plots = [ax2.plot(x, dist_time[idx],label=f'{links}')[0] for idx,(links,joints) in enumerate(joints_dict.items())]
+    plt.legend()
+    title_2 = f'Distances of limbs in frames {args.start_frame}-{args.end_frame}'
 
-# save animation.gif
-if args.output_anim:
-    writergif = animation.PillowWriter(fps=30) 
-    ani1.save(os.path.join(args.data_path,f"anim_{dt.now().strftime('%d-%h-%H-%M')}_{args.data_path.split('/')[1]}.gif"), writer=writergif)
+    ### animation ###
+    ani1 = animation.FuncAnimation(fig,plot_func,frames=N_frames,fargs=(ax1,joints_dict,coords,high_1,low_1,title_1,
+                                                                        ax2,dist_plots,dist_time,title_2),interval=17)
+    plt.show()
+
+    #### save animation.gif ###
+    if args.output_anim:
+        writergif = animation.PillowWriter(fps=30) 
+        ani1.save(os.path.join(args.data_path,f"anim_{dt.now().strftime('%d-%h-%H-%M')}_{args.data_path.split('/')[1]}.gif"), writer=writergif)
--- util/utils.py
+++ util/utils.py
@@ -6,33 +6,30 @@ import numpy as np
 ###### calculate distances ######
 #################################
         
-def calc_dist(coords,joints):
-    dist_sum = np.zeros(coords[0].shape[0])
-    for i,j in zip(joints[:-1],joints[1:]):
-        dist_sum += np.sqrt((coords[0][:,i] - coords[0][:,j])**2 + (coords[1][:,i] - coords[1][:,j])**2 + (coords[2][:,i] - coords[2][:,j])**2)
+def calc_dist(coords,joints_idx):
+    dist_sum = np.zeros(coords.shape[0])
+    for i,j in zip(joints_idx[:-1],joints_idx[1:]):
+        # coords[:,0,i]: all frames, x, i-th joint
+        dist_sum += np.sqrt((coords[:,0,i] - coords[:,0,j])**2 + (coords[:,1,i] - coords[:,1,j])**2 + (coords[:,2,i] - coords[:,2,j])**2)
     return dist_sum
     
 def calc_distances_timeline(coords,joints_dict):
-    dist_time = []
-    for links,joints in joints_dict.items():
-        dist_time.append(calc_dist(coords,joints))
+    # coords: [#frames,3,26]
+    dist_time = np.zeros((len(joints_dict),coords.shape[0])) # dist_time: [5,#frames]
+    # dist_time = []
+    for idx,(_,joints_idx) in enumerate(joints_dict.items()):
+        dist_time[idx,:] = calc_dist(coords,joints_idx)
+    print(f'dist_time shape: {dist_time.shape}')
     return dist_time
 
 ##############################
 ###### plot links in 3D ######
 ##############################
 
-def connect(frame,ax,joints):
-    idx_lst = [[],[],[]]
-    for i in joints:
-        idx_lst[0].append(frame[0][i])
-        idx_lst[1].append(frame[1][i])
-        idx_lst[2].append(frame[2][i])
-        ax.plot(idx_lst[0], idx_lst[1], idx_lst[2], color='red')
-
 def connect_with_lines(frame,ax,joints_dict):
+    # frame: [3,26]
     for links,joints in joints_dict.items():
-        connect(frame,ax,joints)
+        ax.plot(frame[0,joints], frame[1,joints], frame[2,joints], color='red')
 
 #######################################
 ###### plot distance-lines in 2D ######
@@ -58,36 +55,27 @@ def get_ori_data(path,save):
     # original coordinates are different in y and z, so reserve them, both cog and other coordinates
     cog = csv_np[:,3:6]
     cog[:,[2,1]] = cog[:,[1,2]]
-    x_pos_ori = csv_np[:,[6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,102,108,114,120,126,132,138,144,150,168]]
+    x_pos_ori = csv_np[:,[6,12,18,24,30,36,42,48,54,60,66,72,78,84,90,96,102,108,114,120,126,132,138,144,150,168]] # x_pos_ori: [#frames,26]
     z_pos_ori = csv_np[:,[7,13,19,25,31,37,43,49,55,61,67,73,79,85,91,97,103,109,115,121,127,133,139,145,151,169]]
     y_pos_ori = csv_np[:,[8,14,20,26,32,38,44,50,56,62,68,74,80,86,92,98,104,110,116,122,128,134,140,146,152,170]]
     print(f'x_pos shape: {x_pos_ori.shape}')
     
-    # save the original position vectors into csv, or not
-    if save == 'yes':
-        num_row = x_pos_ori.shape[0]
-        num_col = x_pos_ori.shape[1] * 3 + 3
-        output_df = np.zeros((num_row,num_col))
-        for frame,(C,X,Y,Z) in enumerate(zip(cog,x_pos_ori,y_pos_ori,z_pos_ori)):
-            output_df[frame,0:3] = C # center of gravity
-            output_df[frame,3::3] = X # x position
-            output_df[frame,4::3] = Y # y position
-            output_df[frame,5::3] = Z # z position
-        df = pd.DataFrame(output_df)
-        df.to_csv(os.path.join(output_path,'coordinates_yz.csv'),index=False,header=False)
+    coords = np.concatenate((np.expand_dims(x_pos_ori, axis=1),
+                             np.expand_dims(y_pos_ori, axis=1),
+                             np.expand_dims(z_pos_ori, axis=1)), axis=1) # coords: [#frames,3,26]
+    cog = np.expand_dims(cog,axis=1)
+    print(f'coords shape: {coords.shape}')
+    print(f'cog shape: {cog.shape}')
     
-    return cog,x_pos_ori,y_pos_ori,z_pos_ori
-    
+    return cog,coords
+
 def get_prepared(path,frame_range=None):
 
-    cog,x_pos_ori,y_pos_ori,z_pos_ori = get_ori_data(path=path,save='no')
+    cog,coords = get_ori_data(path=path,save='no') 
 
-    # if frame_range is not designated, then return all data
-    if frame_range == None:
-        coords_ori = [x_pos_ori,y_pos_ori,z_pos_ori]
-    # if frame_range is designated, then return the designated ones
-    else:
-        coords_ori = [x_pos_ori[frame_range[0]:frame_range[1]],y_pos_ori[frame_range[0]:frame_range[1]],z_pos_ori[frame_range[0]:frame_range[1]]]
+    # if frame_range is designated, then return designated ones
+    if not frame_range == None:
+        coords = coords[frame_range[0]:frame_range[1]]
     
     # joints list
     joints_dict = {'left arm': [15,0,1,2,14,24],
@@ -97,44 +85,44 @@ def get_prepared(path,frame_range=None):
                   'spine': [20,21,22,23,24,25]
                  }
     # change of distances
-    dist_time = calc_distances_timeline(coords_ori,joints_dict)
+    dist_time = calc_distances_timeline(coords,joints_dict)
     
-    return coords_ori,dist_time,joints_dict
+    return coords,dist_time,joints_dict
 
 ##################################
 ###### plot with each frame ######
 ##################################
 
 def calc_axis_limit(coords):
-    x_high, x_low = int(np.ceil(coords[0].max()*1000/200.0))*200, int(np.floor(coords[0].min()*1000/200.0))*200
-    y_high, y_low = int(np.ceil(coords[1].max()*1000/200.0))*200, int(np.floor(coords[1].min()*1000/200.0))*200
-    z_high, z_low = int(np.ceil(coords[2].max()*1000/200.0))*200, int(np.floor(coords[2].min()*1000/200.0))*200
-    return [[x_high, x_low],[y_high, y_low],[z_high, z_low]]
+    x_high, x_low = int(np.ceil(coords[:,0,:].max()*1000/200.0))*200, int(np.floor(coords[:,0,:].min()*1000/200.0))*200
+    y_high, y_low = int(np.ceil(coords[:,1,:].max()*1000/200.0))*200, int(np.floor(coords[:,1,:].min()*1000/200.0))*200
+    z_high, z_low = int(np.ceil(coords[:,2,:].max()*1000/200.0))*200, int(np.floor(coords[:,2,:].min()*1000/200.0))*200
+    high = np.array([x_high,y_high,z_high])
+    low = np.array([x_low,y_low,z_low])
+    del x_high, x_low, y_high, y_low, z_high, z_low
+    return high,low
 
-def prepare_ax(coords,ax,limits):
+def prepare_ax(ax,high,low):
     # axis label
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
     # axis limit
-    x_high, x_low = limits[0][0],limits[0][1]
-    y_high, y_low = limits[1][0],limits[1][1]
-    z_high, z_low = limits[2][0],limits[2][1]
-    ax.axes.set_xlim3d(left=x_low/1000.0, right=x_high/1000.0) 
-    ax.axes.set_ylim3d(bottom=y_low/1000.0, top=y_high/1000.0)
-    ax.axes.set_zlim3d(bottom=z_low/1000.0, top=z_high/1000.0)
+    ax.axes.set_xlim3d(left=low[0]/1000.0, right=high[0]/1000.0)
+    ax.axes.set_ylim3d(bottom=low[1]/1000.0, top=high[1]/1000.0)
+    ax.axes.set_zlim3d(bottom=low[2]/1000.0, top=high[2]/1000.0)
     # axis scale
-    ax.set_xticks(list(i/1000.0 for i in range(x_low,x_high,200)))
-    ax.set_yticks(list(i/1000.0 for i in range(y_low,y_high,200)))
-    ax.set_zticks(list(i/1000.0 for i in range(z_low,z_high,200)))
+    ax.set_xticks(list(i/1000.0 for i in range(low[0],high[0],200)))
+    ax.set_yticks(list(i/1000.0 for i in range(low[1],high[1],200)))
+    ax.set_zticks(list(i/1000.0 for i in range(low[2],high[2],200)))
     # axis aspect ratio
-    ax.set_box_aspect(aspect = (x_high-x_low,y_high-y_low,z_high-z_low))
+    ax.set_box_aspect(aspect = (high[0]-low[0],high[1]-low[1],high[2]-low[2]))
 
-def plot_func_3d(frame_id,ax,joints_dict,coords,limits,title):
+def plot_func_3d(frame_id,ax,joints_dict,coords,high,low,title):
     # plot links in 3D 
     ax.cla()
-    current_frame = [coords[0][frame_id], coords[1][frame_id], coords[2][frame_id]] # x_pos,z_pos,y_pos
-    prepare_ax(coords,ax,limits)
+    current_frame = coords[frame_id] # current_frame: [3,26]
+    prepare_ax(ax,high,low)
     ax.set_title(title)
     ax.scatter3D(current_frame[0], current_frame[1], current_frame[2], c='steelblue', marker='<')
     connect_with_lines(current_frame,ax,joints_dict)
@@ -148,6 +136,6 @@ def plot_func_2d(frame_id,ax,dist_plots,dist_time,title):
     for idx,dist_plot in enumerate(dist_plots):
         dist_plot.set_data(x, dist_time[idx][:frame_id])
 
-def plot_func(frame_id,ax1,joints_dict,coords_ori,limits_1,title_1,ax2,dist_plots,dist_time,title_2):
-    plot_func_3d(frame_id,ax1,joints_dict,coords_ori,limits_1,title_1)
+def plot_func(frame_id,ax1,joints_dict,coords_ori,high_1,low_1,title_1,ax2,dist_plots,dist_time,title_2):
+    plot_func_3d(frame_id,ax1,joints_dict,coords_ori,high_1,low_1,title_1)
     plot_func_2d(frame_id,ax2,dist_plots,dist_time,title_2)
