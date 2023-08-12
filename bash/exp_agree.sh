68d68ecd8f44bd007f1d44977eb45fd51ec9e60d
hardli <1060111768@qq.com>
2023-08-12 16:32:56 +0200

    update bash

--- bash/exp_agree.sh
+++ bash/exp_agree.sh
@@ -8,55 +8,18 @@ model="$1" # which model to train
 ext="$2" # 0: not use external testset, 1: use external testset
 wl="$3" # window size
 
-if [[ $model == "KNN" ]]; then
-
-    python train_dynamic_ML.py \
-        --train_split_method_paths "dataset/agree_20230801/split_method.yaml" \
-        --trainset_path "dataset/agree_20230801/x_data_UpperLowerBody.npy" \
-                        "dataset/agree_20230801/y_data_UpperLowerBody.npy" \
-        --test_split_method_paths "dataset/agree_test_20230801/split_method.yaml" \
-        --testset_path "dataset/agree_test_20230801/x_data_UpperLowerBody.npy" \
-                       "dataset/agree_test_20230801/y_data_UpperLowerBody.npy" \
-        --split_ratio 0.8 \
-        --window_size $wl \
-        --model "KNN" \
-        --n_neighbor 20 \
-        --exp_group "Agree" \
-        --outside_test $ext \
-        --save_res 1
-
-elif [[ $model == "RandomForest" ]]; then
-
-    python train_dynamic_ML.py \
-        --train_split_method_paths "dataset/agree_20230801/split_method.yaml" \
-        --trainset_path "dataset/agree_20230801/x_data_UpperLowerBody.npy" \
-                        "dataset/agree_20230801/y_data_UpperLowerBody.npy" \
-        --test_split_method_paths "dataset/agree_test_20230801/split_method.yaml" \
-        --testset_path "dataset/agree_test_20230801/x_data_UpperLowerBody.npy" \
-                       "dataset/agree_test_20230801/y_data_UpperLowerBody.npy" \
-        --split_ratio 0.8 \
-        --window_size $wl \
-        --model "RandomForest" \
-        --max_depth 6 \
-        --random_state 0 \
-        --exp_group "Agree" \
-        --outside_test $ext \
-        --save_res 1
-
-elif [[ $model == "SVM" ]]; then
-
-    python train_dynamic_ML.py \
-        --train_split_method_paths "dataset/agree_20230801/split_method.yaml" \
-        --trainset_path "dataset/agree_20230801/x_data_UpperLowerBody.npy" \
-                        "dataset/agree_20230801/y_data_UpperLowerBody.npy" \
-        --test_split_method_paths "dataset/agree_test_20230801/split_method.yaml" \
-        --testset_path "dataset/agree_test_20230801/x_data_UpperLowerBody.npy" \
-                       "dataset/agree_test_20230801/y_data_UpperLowerBody.npy" \
-        --split_ratio 0.8 \
-        --window_size $wl \
-        --model "SVM" \
-        --exp_group "Agree" \
-        --outside_test $ext \
-        --save_res 1
-
-fi
+python train.py \
+    --train_split_method_paths "dataset/agree_20230801/split_method.yaml" \
+    --trainset_path "dataset/agree_20230801/x_data_UpperLowerBody.npy" \
+                    "dataset/agree_20230801/y_data_UpperLowerBody.npy" \
+    --test_split_method_paths "dataset/agree_test_20230801/split_method.yaml" \
+    --testset_path "dataset/agree_test_20230801/unknown.NoHead.csv" \
+    --split_ratio 0.9 \
+    --window_size $wl \
+    --model $model \
+    --max_depth 6 \
+    --random_state 0 \
+    --n_neighbor 20 \
+    --exp_group "Agree" \
+    --outside_test $ext \
+    --save_res 1
--- bash/exp_dynamic.sh
+++ bash/exp_dynamic.sh
@@ -8,61 +8,20 @@ model="$1" # which model to train
 ext="$2" # 0: not use external testset, 1: use external testset
 wl="$3" # window size
 
-if [[ $model == "KNN" ]]; then
-
-    python train_dynamic_ML.py \
-        --train_split_method_paths "dataset/dynamic1_20230706/split_method.yaml" \
-                                   "dataset/dynamic2_20230706/split_method.yaml" \
-                                   "dataset/dynamic3_20230706/split_method.yaml" \
-        --trainset_path "dataset/dynamic_dataset/x_data_UpperLowerBody.npy" \
-                        "dataset/dynamic_dataset/y_data_UpperLowerBody.npy" \
-        --test_split_method_paths "dataset/dynamic_test_20230801/split_method.yaml" \
-        --testset_path "dataset/dynamic_test_20230801/x_data_UpperLowerBody.npy" \
-                       "dataset/dynamic_test_20230801/y_data_UpperLowerBody.npy" \
-        --split_ratio 0.8 \
-        --window_size $wl \
-        --model "KNN" \
-        --n_neighbor 20 \
-        --exp_group "Dynamic" \
-        --outside_test $ext \
-        --save_res 1
-
-elif [[ $model == "RandomForest" ]]; then
-
-    python train_dynamic_ML.py \
-        --train_split_method_paths "dataset/dynamic1_20230706/split_method.yaml" \
-                                   "dataset/dynamic2_20230706/split_method.yaml" \
-                                   "dataset/dynamic3_20230706/split_method.yaml" \
-        --trainset_path "dataset/dynamic_dataset/x_data_UpperLowerBody.npy" \
-                        "dataset/dynamic_dataset/y_data_UpperLowerBody.npy" \
-        --test_split_method_paths "dataset/dynamic_test_20230801/split_method.yaml" \
-        --testset_path "dataset/dynamic_test_20230801/x_data_UpperLowerBody.npy" \
-                       "dataset/dynamic_test_20230801/y_data_UpperLowerBody.npy" \
-        --split_ratio 0.8 \
-        --window_size $wl \
-        --model "RandomForest" \
-        --max_depth 6 \
-        --random_state 0 \
-        --exp_group "Dynamic" \
-        --outside_test $ext \
-        --save_res 1
-
-elif [[ $model == "SVM" ]]; then
-
-    python train_dynamic_ML.py \
-        --train_split_method_paths "dataset/dynamic1_20230706/split_method.yaml" \
-                                   "dataset/dynamic2_20230706/split_method.yaml" \
-                                   "dataset/dynamic3_20230706/split_method.yaml" \
-        --trainset_path "dataset/dynamic_dataset/x_data_UpperLowerBody.npy" \
-                        "dataset/dynamic_dataset/y_data_UpperLowerBody.npy" \
-        --test_split_method_paths "dataset/dynamic_test_20230801/split_method.yaml" \
-        --testset_path "dataset/dynamic_test_20230801/x_data_UpperLowerBody.npy" \
-                       "dataset/dynamic_test_20230801/y_data_UpperLowerBody.npy" \
-        --split_ratio 0.8 \
-        --window_size $wl \
-        --model "SVM" \
-        --exp_group "Dynamic" \
-        --outside_test $ext \
-        --save_res 1
-
-fi
+python train.py \
+    --train_split_method_paths "dataset/dynamic1_20230706/split_method.yaml" \
+                                "dataset/dynamic2_20230706/split_method.yaml" \
+                                "dataset/dynamic3_20230706/split_method.yaml" \
+    --trainset_path "dataset/dynamic_dataset/x_data_UpperLowerBody.npy" \
+                    "dataset/dynamic_dataset/y_data_UpperLowerBody.npy" \
+    --test_split_method_paths "dataset/dynamic_test_20230801/split_method.yaml" \
+    --testset_path "dataset/dynamic_test_20230801/unknown.NoHead.csv" \
+    --split_ratio 0.8 \
+    --window_size $wl \
+    --model $model \
+    --max_depth 6 \
+    --random_state 0 \
+    --n_neighbor 20 \
+    --exp_group "Dynamic" \
+    --outside_test $ext \
+    --save_res 1
